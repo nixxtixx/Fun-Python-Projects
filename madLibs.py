4396b7def56e7972fc149e7d41ae046e96a009a8
nixxtixx <51759355+nixxtixx@users.noreply.github.com>
2020-07-10 01:09:12 -0400

    Upload Commit
    Simple Mad Libs program

---
+++ madLibs.py
@@ -0,0 +1,5 @@
+Noun = input("Type a noun: ")
+adjective = input("Type an adjective: ")
+Verb = input("Type a Verb")
+
+print( Noun + " really likes summer because it is " +  adjective , Verb + " is my favorite thing to do")
