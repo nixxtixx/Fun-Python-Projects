819b8187d3089646879d4ab6473c3882e051aa67
Nina Tippett <51759355+nixxtixx@users.noreply.github.com>
2020-07-10 01:18:45 -0400

    Upload Commit
    Checking to see if your favorite color is in the rainbow

---
+++ colors.py
@@ -0,0 +1,19 @@
+color = input("Whats your favorite color? ")
+
+
+if color == "Red" or color == "red":
+  print(color + " is part of the rainbow")
+elif color == "Orange" or color == "orange":
+  print(color + " is part of the rainbow")
+elif color == "Yellow" or color == "yellow":
+  print(color + " is part of the rainbow")
+elif color == "Green" or color == "green":
+  print(color + " is part of the rainbow")
+elif color == "Blue" or color == "blue":
+  print(color + " is part of the rainbow")
+elif color == "Indigo" or color == "indigo":
+  print(color + " is part of the rainbow")
+elif color == "Violet" or color == "violet":
+  print(color + " is part of the rainbow")
+else:
+  print("Invalid Word")
